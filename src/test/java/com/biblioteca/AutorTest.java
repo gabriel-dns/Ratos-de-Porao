2e3d43f6b1179f799571386766f672b9dc16b624
Gabriel Nascimento <gabrielsn039@gmail.com>
2023-09-14 22:07:01 -0300

    ajuste teste

--- src/test/java/com/biblioteca/AutorTest.java
+++ src/test/java/com/biblioteca/AutorTest.java
@@ -12,7 +12,7 @@ public class AutorTest {
          */
         @Test
         public void testConstrutorComParametros() {
-                String nome = null;
+                String nome = "João";
                 String sobrenome = "Silva";
                 Autor autor = new Autor(nome, sobrenome);
                 Assert.assertNotNull(autor.nome);
